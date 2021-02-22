35a805c1aaf2270e77f04b5ea0548cf0228a9800
HyeongchaeSon <HyeongchaeSon>
2021-02-22 16:16:32 +0900

    add viewDidDisappear.

--- Upper/ViewController.swift
+++ Upper/ViewController.swift
@@ -14,7 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     override func viewWillAppear(_ animated: Bool) {
-        <#code#>
+        //
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        //
     }
 }
-
